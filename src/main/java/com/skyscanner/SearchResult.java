512bf7bbe2286d9a3f646ff70d84a91f5916e795
vishwa.vj <iamvishwajith.s@gmail.com>
2025-04-09 01:14:53 +0530

    Implemented microservice

---
+++ src/main/java/com/skyscanner/SearchResult.java
@@ -0,0 +1,34 @@
+
+package com.skyscanner;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SearchResult {
+    @JsonProperty
+    private String city;
+
+    @JsonProperty
+    private String kind;
+
+    @JsonProperty
+    private String title;
+
+    public SearchResult(){
+
+    }
+
+    public SearchResult(String city, String title, String kind){
+        this.city = city;
+        this.title = title;
+        this.kind = kind;
+    }
+    public String getCity(){
+        return city;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getKind(){
+        return kind;
+    }
+}
